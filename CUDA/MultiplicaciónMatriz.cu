64819de70381009546b44d46e796d40490cf6121
Magoratogha <magoratoga@gmail.com>
2018-04-12 15:24:00 -0500

    Algunos fixes

--- CUDA/MultiplicaciónMatriz.cu
+++ CUDA/MultiplicaciónMatriz.cu
@@ -4,12 +4,10 @@
 #include <cuda.h>
 
 __global__
-void MulMatriz(float* d_min, float* d_mout, int fil, int col)
+void MulMatriz(float *d_m, int fil, int col, int n)
 {
-	int i = threadIdx.y + blockDim.y * blockIdx.y;
-	int j= threadIdx.x + blockDim.x * blockIdx.x;
-
-	if((i<fil)&&(j<col)) d_mout[i*col+j] = d_min[i*col+j]*5; 
+	int i = threadIdx.x + blockDim.x * blockIdx.x;
+	if(i<fil*col) d_m[i] = d_m[i]*n; 
 }
 
 
@@ -22,19 +20,18 @@ int main()
   	//-------------------------------------
 
 	int fil, col;
-	float *h_min, *h_mout;
-	float *d_min, *d_mout;
+	float* h_m;
+	float* d_m;
 
 	fil = 3;
 	col = 4; //con el más grande se hace la referencia para la matriz en 1D
 
-	h_min = (float*)malloc (fil*col*sizeof(float)); //Reserva de memoria en el host
-	h_mout = (float*)malloc (fil*col*sizeof(float));
+	h_m = (float *)malloc (fil*col*sizeof(float *)); //Reserva de memoria en el host
 
 	//Iniciar matriz con valor 13------------------
 	for(int i=0; i<fil; i++){
 		for(int j=0; j<col; j++){
-			h_min[i*col+j] = 13; 
+			h_m[i*col+j] = 13; 
 		}
 	}
 
@@ -43,7 +40,7 @@ int main()
 
 	for(int i=0; i<fil; i++){
 		for(int j=0; j<col; j++){
-			printf("%f ", h_min[i*col+j]);
+			printf("%f ", h_m[i*col+j]);
 		}
 		printf("\n"); 
 	}	
@@ -52,31 +49,25 @@ int main()
 
 	//Reserva y copia de datos al dispositivo ---------
 	int size = fil*col*sizeof(float);
-	int blockSize = 32;
-    dim3 dimBlock(blockSize, blockSize, 1);
-    dim3 dimGrid(ceil(col/float(blockSize)), ceil(fil/float(blockSize)), 1);
-	
-	cudaMalloc((void **) &d_min, size);
-	cudaMalloc((void **) &d_mout, size);	
-	cudaMemcpy(d_min, h_min, size, cudaMemcpyHostToDevice);
+	cudaMalloc((void **) &d_m, size);
+	cudaMemcpy(d_m, h_m, size, cudaMemcpyHostToDevice);
 	//-------------------------------------------------
 
-	MulMatriz<<<dimGrid,dimBlock>>>(d_min, d_mout, fil, col); //Ejecución del kernel
+	MulMatriz<<<ceil(fil*col/256.0),256>>>(d_m, fil, col, 5); //Ejecución del kernel
 
-	cudaMemcpy(h_mout, d_mout, size, cudaMemcpyDeviceToHost); //Copia de datos al host
+	cudaMemcpy(h_m, d_m, size, cudaMemcpyDeviceToHost); //Copia de datos al host
+	cudaFree(d_m); //Liberar memoria del dispositivo
 
 	//Imprimir resultados------------------
 	for(int i=0; i<fil; i++){
 		for(int j=0; j<col; j++){
-			printf("%f ", h_mout[i*col+j]);
+			printf("%f ", h_m[i*fil+j]);
 		}
 		printf("\n"); 
 	}
 	//-------------------------------------
-	cudaFree(d_min); //Liberar memoria del dispositivo
-	cudaFree(d_mout);
-	free(h_min);//Liberar memoria host
-	free(h_mout);
+
+	free(h_m);//Liberar memoria host
 
 	//Fin reloj ------------------------
   	t_fin = clock();
