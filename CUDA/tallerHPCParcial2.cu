1de2733fb5f92542f90893b458ae64dbea55f4c4
Magoratogha <magoratoga@gmail.com>
2018-04-12 17:27:42 -0500

    Agregado taller parcial 2

--- CUDA/tallerHPCParcial2.cu
+++ CUDA/tallerHPCParcial2.cu
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <cuda.h>
 
-__global__ void MulMatriz(float *m1, float *m2, float *mr, int fil, int col)
+__global__ void MulMatriz(float *m1, float *m2, float *mr, int fil, int col, int w)
 {
 	int i = blockIdx.y * blockDim.y + threadIdx.y;
     int j = blockIdx.x * blockDim.x + threadIdx.x;
@@ -11,10 +11,10 @@ __global__ void MulMatriz(float *m1, float *m2, float *mr, int fil, int col)
 
     if ((i<fil) && (j<col)){
     	valor = 0;
-    	for(int k=0; k<col; k++){
-    		valor = valor + m1[i*col+k] * m2[k*col+j];
+    	for(int k=0; k<w; k++){
+    		valor = valor + m1[i*w+k] * m2[k*w+j];
     	}
-        mr[i*col+j] = valor; 
+        mr[i*w+j] = valor; 
     }
 }
 
@@ -87,7 +87,7 @@ int main()
 	cudaMemcpy(d_m1, h_m1, size1, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_m2, h_m2, size2, cudaMemcpyHostToDevice);
 
-	MulMatriz<<<dimGrid, dimBlock>>>(d_m1, d_m2, d_mr, fil1, col2); //Ejecución del kernel
+	MulMatriz<<<dimGrid, dimBlock>>>(d_m1, d_m2, d_mr, fil1, col2, col1); //Ejecución del kernel
 	cudaMemcpy(h_mr, d_mr, sizer, cudaMemcpyDeviceToHost); //Copia de datos al host
 	
 	//Imprimir resultados------------------
