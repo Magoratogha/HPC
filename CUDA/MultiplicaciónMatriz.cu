8445ec0b7ba66fc7063186dbaa311a4c9d70e9d8
Magoratogha <magoratoga@gmail.com>
2018-04-12 14:59:38 -0500

    Algunos fixes

--- CUDA/MultiplicaciónMatriz.cu
+++ CUDA/MultiplicaciónMatriz.cu
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <cuda.h>
-#include <cuda_runtime.h>
 
 __global__
 void MulMatriz(float *d_m, int fil, int col, int n)
 {
-	int i = threadIdx.x + blockDim.x * blockIdx.x;
-	if(i<fil*col) d_m[i] = d_m[i]*n; 
+	int i = threadIdx.y + blockDim.y * blockIdx.y;
+	int j= threadIdx.x + blockDim.x * blockIdx.x;
+
+	if((i<fil)&&(j<col)) d_m[i*col+j] = d_m[i*col+j]*n; 
 }
 
 
@@ -21,20 +22,18 @@ int main()
   	//-------------------------------------
 
 	int fil, col;
-	float* h_in;
-	float* h_out;
-	float* d_m;
+	float *h_m;
+	float *d_m;
 
 	fil = 3;
 	col = 4; //con el más grande se hace la referencia para la matriz en 1D
 
-	h_in = (float *)malloc (fil*col*sizeof(float *)); //Reserva de memoria en el host
-	h_out = (float *)malloc (fil*col*sizeof(float *));
+	h_m = (float*)malloc (fil*col*sizeof(float *)); //Reserva de memoria en el host
 
 	//Iniciar matriz con valor 13------------------
 	for(int i=0; i<fil; i++){
 		for(int j=0; j<col; j++){
-			h_in[i*col+j] = 13; 
+			h_m[i*col+j] = 13; 
 		}
 	}
 
@@ -43,7 +42,7 @@ int main()
 
 	for(int i=0; i<fil; i++){
 		for(int j=0; j<col; j++){
-			printf("%f ", h_in[i*col+j]);
+			printf("%f ", h_m[i*col+j]);
 		}
 		printf("\n"); 
 	}	
@@ -53,25 +52,24 @@ int main()
 	//Reserva y copia de datos al dispositivo ---------
 	int size = fil*col*sizeof(float);
 	cudaMalloc((void **) &d_m, size);
-	cudaMemcpy(d_m, h_in, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_m, h_m, size, cudaMemcpyHostToDevice);
 	//-------------------------------------------------
 
 	MulMatriz<<<ceil(fil*col/256.0),256>>>(d_m, fil, col, 5); //Ejecución del kernel
 
-	cudaMemcpy(h_out, d_m, size, cudaMemcpyDeviceToHost); //Copia de datos al host
-	//Liberar memoria del dispositivo
+	cudaMemcpy(h_m, d_m, size, cudaMemcpyDeviceToHost); //Copia de datos al host
+	cudaFree(d_m); //Liberar memoria del dispositivo
 
 	//Imprimir resultados------------------
 	for(int i=0; i<fil; i++){
 		for(int j=0; j<col; j++){
-			printf("%f ", h_out[i*fil+j]);
+			printf("%f ", h_m[i*fil+j]);
 		}
 		printf("\n"); 
 	}
 	//-------------------------------------
-	cudaFree(d_m);
-	free(h_in);
-	free(h_out);//Liberar memoria host
+
+	free(h_m);//Liberar memoria host
 
 	//Fin reloj ------------------------
   	t_fin = clock();
