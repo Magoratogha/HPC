6e7a1300ae597d9e80978e4cb1a22d237400a275
Magoratogha <magoratoga@gmail.com>
2018-04-12 20:41:21 -0500

    Agregada lectura de archivos

--- CUDA/Taller Parcial 2/taller2.sh
+++ CUDA/Taller Parcial 2/taller2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=MulMatrix
-#SBATCH --ouput=res_MulMatrix.out
+#SBATCH --output=res_MulMatrix.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=10:00
