7c74e1d8af9d41f652f03b5e486d02499a9d980f
Magoratogha <magoratoga@gmail.com>
2018-04-12 21:03:23 -0500

    Agregado el batch de slurm

--- CUDA/Taller Parcial 2/taller2.sh
+++ CUDA/Taller Parcial 2/taller2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+
+#SBATCH --job-name=matrixmul
+#SBATCH --output=matrixmul.out
 #
-#SBATCH --job-name=MulMatrix
-#SBATCH --output=res_MulMatrix.out
-#SBATCH --ntasks=1
-#SBATCH --nodes=3
-#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
 #SBATCH --time=10:00
-#SBATCH --mem-per-cpu=500
+#SBATCH --mem-per-cpu=100
 
 ./out input.txt
+sleep 60
