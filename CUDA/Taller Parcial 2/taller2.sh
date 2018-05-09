df0d8f950eea3f8ae33e354c7db564a8be2a5b66
magoratogha <magoratoga@gmail.com>
2018-05-09 17:49:13 -0500

    bash actualizado

--- CUDA/Taller Parcial 2/taller2.sh
+++ CUDA/Taller Parcial 2/taller2.sh
@@ -9,7 +9,7 @@
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 ./out1 input-long.txt
 rm outputSecuencial.txt
