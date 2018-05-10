3f7cc242b1888acbd3fad4e5a16027d00b142832
magoratogha <magoratoga@gmail.com>
2018-05-10 13:34:18 -0500

    Algunas pruebas

--- CUDA/Taller Parcial 2/prueba-mid.sh
+++ CUDA/Taller Parcial 2/prueba-mid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=MatrixMul
-#SBATCH --output=MatrixMul.out
+#SBATCH --output=MatrixMul-Mid.out
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
@@ -9,7 +9,7 @@
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 ./out1 input-mid.txt
 rm outputSecuencial.txt
